3196ff4f4f5397721505945bfefd8c513beb49af
maksmanus <mozgov111poise@gmail.com>
2025-03-12 10:36:18 +0900

    2

---
+++ js/CheckGithub.js
@@ -0,0 +1,11 @@
+const _https = "https://"
+
+function SetHomeURL()
+{
+    if(window.location.hostname("github.io"))
+    {
+        let hostname = window.location.hostname
+        let link = document.getElementById("chaeckGit");
+        link.setAttribute("href", _https + window.location.)
+    }
+}
