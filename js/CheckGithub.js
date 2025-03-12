f0d17cea32c7a8eea017c5bf82bfad2b6394e2fc
maksmanus <mozgov111poise@gmail.com>
2025-03-12 11:28:50 +0900

    Fixed Home Button

--- js/CheckGithub.js
+++ js/CheckGithub.js
@@ -2,10 +2,19 @@ const _https = "https://"
 
 function SetHomeURL()
 {
-    if(window.location.hostname("github.io"))
+    if(window.location.host.includes("github.io"))
     {
         let hostname = window.location.hostname
         let link = document.getElementById("chaeckGit");
-        link.setAttribute("href", _https + window.location.)
+        let string = _https + window.location.host + '/' + "ssmp-docs/";
+        link.setAttribute("href", string);
+        console.log(string);
+    }
+
+    else
+    {
+        console.log(_https + window.location.host);
     }
 }
+
+document.addEventListener("DOMContentLoaded", SetHomeURL)
